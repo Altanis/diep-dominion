a41b3902861a8b7353b79678886d2ad04ad2c635
CoderSudaWuda <38045884+CoderSudaWuda@users.noreply.github.com>
2020-07-01 15:13:09 -0400

    lockdown system

---
+++ commands/lockdown.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'lockdown',
+    aliases: [],
+    description: 'Locks a channel down, disabling everyone except Administrators from typing.',
+    usage: '::lockdown [channel/"all"] --default:sent-channel',
+    category: 'mod',
+    run: async (client, msg, args) => {
+        const PureEmbed = new client.Embedder();
+        
+        if (!client.checkPermission(msg.member, 'MANAGE_CHANNELS')) return PureEmbed.ErrorEmbed('You must have the `Manage Channels` permission to execute this command.');
+        
+        const channel = msg.mentions.channels.first() || msg.channel;
+        
+        if (args[0] == 'all') {
+            PureEmbed.LoadingEmbed(`Locking down all channels...`).then(async emb => {
+                ['712044704379699241', '712044762894565457', '712044826568294440'].map(id => {
+                    const channel = msg.guild.channels.cache.get(id);
+    
+                    channel.updateOverwrite(msg.guild.roles.everyone, {
+                        SEND_MESSAGES: false,
+                    });
+
+                    channel.setName(`${channel.name}-🔒`)
+                });
+
+                emb.delete();
+                PureEmbed.SuccessEmbed(`Locked down all channels successfully!`);
+            });
+        } else {
+            PureEmbed.LoadingEmbed(`Locking down <#${channel.id}>...`).then(async emb => {
+                channel.updateOverwrite(msg.guild.roles.everyone, {
+                    SEND_MESSAGES: false,
+                }).then(channel => channel.setName(`${channel.name}-🔒`));
+
+                emb.delete()
+                PureEmbed.SuccessEmbed(`Locked down <#${channel.id}> successfully!`)
+            });
+        }
+    },
+};
---
+++ commands/unlock.js
@@ -0,0 +1,40 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'unlock',
+    aliases: [],
+    description: 'Unlocks  a locked channel.',
+    usage: '::unlock [channel/"all"] --default:sent-channel',
+    category: 'mod',
+    run: async (client, msg, args) => {
+        const PureEmbed = new client.Embedder();
+
+        if (!client.checkPermission(msg.member, 'MANAGE_CHANNELS')) return PureEmbed.ErrorEmbed('You must have the `Manage Channels` permission to execute this command.');
+
+        const channel = msg.mentions.channels.first() || msg.channel;
+        
+        if (args[0] == 'all') {
+            ['712044704379699241', '712044762894565457', '712044826568294440'].map(id => {
+                const channel = msg.guild.channels.cache.get(id);
+
+                channel.updateOverwrite(msg.guild.roles.everyone, {
+                    SEND_MESSAGES: true,
+                });
+            });
+
+            channel.setName(channel.name.replace('-🔒', ''));
+
+            emb.delete();
+            PureEmbed.SuccessEmbed(`Locked down all channels successfully!`);
+        } else {
+            PureEmbed.LoadingEmbed(`Unlocking <#${channel.id}>...`).then(async emb => {
+                channel.updateOverwrite(msg.guild.roles.everyone, {
+                    SEND_MESSAGES: true,
+                }).then(channel => channel.setName(channel.name.replace('-🔒', '')));
+
+                emb.delete();
+                PureEmbed.SuccessEmbed(`Successfully unlocked <#${channel.id}>!`);
+            });
+        }
+    },
+};
