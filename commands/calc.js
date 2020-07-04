1c171c9d1923692bad69e4382e8f9656b625dd57
CoderSudaWuda <38045884+CoderSudaWuda@users.noreply.github.com>
2020-07-04 16:36:35 -0400

    calculator support

---
+++ commands/calc.js
@@ -0,0 +1,21 @@
+const mathjs = require('mathjs');
+
+module.exports = {
+    name: 'calc',
+    aliases: [],
+    description: 'Returns the value from an expression.',
+    category: 'utility',
+    run: async (client, msg, args) => {
+        const PureEmbed = new client.Embedder();
+
+        if (!args[0]) return PureEmbed.ErrorEmbed('No expression was inputted.');
+
+        try {
+            const evalled = mathjs.evaluate(args.join(' '));
+
+            PureEmbed.SuccessEmbed(`The result to your expression is: **${evalled}**`)
+        } catch (error) {
+            PureEmbed.ErrorEmbed(`The expression you inputted was unable to be parsed. \nError: \`${error.message}\``)
+        }
+    },
+};
